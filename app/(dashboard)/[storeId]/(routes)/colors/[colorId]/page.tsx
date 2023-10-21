8eeae0705615598d4b75b5df1ed7851d9703b680
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-10-20 18:21:48 -0700

    ✨ feat(admin): Colors Entity

---
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/page.tsx
@@ -0,0 +1,35 @@
+import prismadb from "@/lib/prismadb";
+
+import { ColorForm } from "./_components/color-form";
+
+const ColorPage = async ({
+  params,
+}: {
+  params: { colorId: string; storeId: string };
+}) => {
+  let color;
+
+  if (params.colorId === "new") {
+    color = await prismadb.color.findUnique({
+      where: {
+        id: params.storeId,
+      },
+    });
+  } else {
+    color = await prismadb.color.findUnique({
+      where: {
+        id: params.colorId,
+      },
+    });
+  }
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ColorForm initialData={color} />
+      </div>
+    </div>
+  );
+};
+
+export default ColorPage;
