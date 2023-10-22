dcc7f9ba8fe1634dc4875211d55d0434e9c0b9ed
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-10-22 12:33:15 -0700

    🐞 fix: fix now props

--- components/modals/alert-modal.tsx
+++ components/modals/alert-modal.tsx
@@ -31,7 +31,7 @@ export const AlertModal: React.FC<AlertModalProps> = ({
   return (
     <Modal
       title="Are you sure?"
-      description="This action cannot be undone."
+      descripcion="This action cannot be undone."
       isOpen={isOpen}
       onClose={onClose}>
       <div className="pt-6 space-x-2 flex items-center justify-end w-full">
